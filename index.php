ffadfa4b7997d3281886ca855a1d526f7f7d773b
anti-matrix <116738035+anti-matrix@users.noreply.github.com>
2022-10-28 23:30:34 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <meta name="viewport" content="width=device-width">
         <title>Vermelho de Paixão 2.0</title>
-        <link rel="icon" type="image/x-icon" href="http://www.vermelhodepaixao.com.br/favicon.ico">
+        <link rel="icon" type="image/x-icon" href="imgs/favicon.ico">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
         <link href="css/style.css" rel="stylesheet"/>
