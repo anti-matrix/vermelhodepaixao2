d15cee8c8c18b9250dc41f002377680e7d211de6
Anti-Matrix <116738035+anti-matrix@users.noreply.github.com>
2022-11-09 21:59:27 -0300

    Add files via upload

---
+++ vermelhodepaixao2/scripts/quemsomos.js
@@ -0,0 +1,20 @@
+z[0]={_id:5531, _titulo:'Quem Somos', _conteudo:'O texto a seguir foi o primeiro post do Vermelho de Paixão, escrito em 28 de dezembro de 2008.
+
+Depois de quase quatro anos acompanhando o América de Natal diretamente na condução do site http://www.americadenatal.com.br/, resolvi em maio deste ano que estava chegando a hora de passar o bastão para outros abnegados. Só que após alguns meses, acho que o descanso já foi o bastante. Vou novamente falar as coisas do MAIOR CLUBE DE FUTEBOL DO RIO GRANDE DO NORTE. Agora através de um blog. O meu pouco tempo disponível, não me concede o direito de regressar ao site oficial do clube. Por isso, vou iniciar está nova caminhada agora. Que 2009 seja o ano do Mecão e que o RN fique cada vez mais VERMELHO, VERMELHO DE PAIXÃO.
+
+Postado por Sérgio Fraiman'};
+z[0]={_id:1011, _titulo:'Quem Somos', _conteudo:'O texto a seguir foi o primeiro post do Vermelho de Paixão, escrito em 28 de dezembro de 2008.
+
+Depois de quase quatro anos acompanhando o América de Natal diretamente na condução do site http://www.americadenatal.com.br/, resolvi em maio deste ano que estava chegando a hora de passar o bastão para outros abnegados. Só que após alguns meses, acho que o descanso já foi o bastante. Vou novamente falar as coisas do MAIOR CLUBE DE FUTEBOL DO RIO GRANDE DO NORTE. Agora através de um blog. O meu pouco tempo disponível, não me concede o direito de regressar ao site oficial do clube. Por isso, vou iniciar está nova caminhada agora. Que 2009 seja o ano do Mecão e que o RN fique cada vez mais VERMELHO, VERMELHO DE PAIXÃO.
+
+Postado por Sérgio Fraiman'};
+z[0]={_id:4175, _titulo:'Quem Somos', _conteudo:'O texto a seguir foi o primeiro post do Vermelho de Paixão, escrito em 28 de dezembro de 2008.
+
+Depois de quase quatro anos acompanhando o América de Natal diretamente na condução do site http://www.americadenatal.com.br/, resolvi em maio deste ano que estava chegando a hora de passar o bastão para outros abnegados. Só que após alguns meses, acho que o descanso já foi o bastante. Vou novamente falar as coisas do MAIOR CLUBE DE FUTEBOL DO RIO GRANDE DO NORTE. Agora através de um blog. O meu pouco tempo disponível, não me concede o direito de regressar ao site oficial do clube. Por isso, vou iniciar está nova caminhada agora. Que 2009 seja o ano do Mecão e que o RN fique cada vez mais VERMELHO, VERMELHO DE PAIXÃO.
+
+Postado por Sérgio Fraiman'};
+z[0]={_id:3105, _titulo:'Quem Somos', _conteudo:'O texto a seguir foi o primeiro post do Vermelho de Paixão, escrito em 28 de dezembro de 2008.
+
+Depois de quase quatro anos acompanhando o América de Natal diretamente na condução do site http://www.americadenatal.com.br/, resolvi em maio deste ano que estava chegando a hora de passar o bastão para outros abnegados. Só que após alguns meses, acho que o descanso já foi o bastante. Vou novamente falar as coisas do MAIOR CLUBE DE FUTEBOL DO RIO GRANDE DO NORTE. Agora através de um blog. O meu pouco tempo disponível, não me concede o direito de regressar ao site oficial do clube. Por isso, vou iniciar está nova caminhada agora. Que 2009 seja o ano do Mecão e que o RN fique cada vez mais VERMELHO, VERMELHO DE PAIXÃO.
+
+Postado por Sérgio Fraiman'};
